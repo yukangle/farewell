de1234ef603046a4e43b0f23bf012601ace01f68
Tom Yu <yukangle@outlook.com>
2021-08-25 21:25:09 +0800

    add upload function

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const app = express();
 const oneDay = 1000 * 60 * 60 * 24;
 const INVITATIONCODE = "123456";
 
+function uuid() {
+  var s = [];
+  var hexDigits = "0123456789abcdef";
+  for (var i = 0; i < 36; i++) {
+      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+  }
+  s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = "-";
+
+  var uuid = s.join("");
+  return uuid;
+}
+
 app.use(sessions({
   secret: "privatesessionsecret-kangleyu",
   saveUninitialized: true,
@@ -62,6 +76,17 @@ app.post('/home', (req, res) => {
   }
 });
 
+app.get('/new', (req, res) => {
+  console.log('/new');
+  session = req.session;
+  if (session.userid) {
+    res.cookie("username", session.userid);
+    res.status(301).sendFile('public/new.html', { root: __dirname });
+  } else {
+    res.sendFile('public/login.html', { root: __dirname });
+  }
+});
+
 app.get('/logout', (req, res) => {
   console.log('/logout');
   req.session.destroy();
@@ -86,25 +111,45 @@ app.get('/cards', (req, res) => {
 
 app.post('/upload', async (req, res) => {
   try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: 'no file uploaded'
-      });
-    } else {
-      let photo = req.files.photo;
-      photo.mv('./data/images/' + photo.name);
+    console.log('/upload');
+    session = req.session;
+    if (session.userid) {
+      res.cookie("username", session.userid);
+      if (!req.files) {
+        res.status(500).sendFile('public/500.html', { root: __dirname });
+      } else {
+        let photo = req.files.photo;
+        var newCard = { "username": session.userid };
+        newCard.descritpion = req.body.sentence;
+        newCard.imageId = photo.name;
+        newCard.date = (new Date(Date.now())).toDateString();
+        newCard.id = uuid();
 
-      res.send({
-        status: true,
-        message: 'file is uploaded',
-        data: {
-          name: photo.name,
-          mimetype: photo.mimetype,
-          size: photo.size
-        }
-      });
+        fs.readFile('./data/data.json', 'utf8', (err, data) => {
+
+          if (err) {
+              console.log(`Error reading file from disk: ${err}`);
+              res.status(500).sendFile('public/500.html', { root: __dirname });
+          } else {
+              const databases = JSON.parse(data);
+              databases.push(newCard);
+              fs.writeFile('./data/data.json', JSON.stringify(databases, null, 4), (err) => {
+                  if (err) {
+                      console.log(`Error writing file: ${err}`);
+                      res.status(500).sendFile('public/500.html', { root: __dirname });
+                  }
+              });
+          }
+        });
+        photo.mv('./data/images/' + photo.name);
+
+        res.status(301).sendFile('public/home.html', { root: __dirname });
+      }
+
+    } else {
+      res.sendFile('public/login.html', { root: __dirname });
     }
+    
   } catch(err) {
     res.status(500).send(err);
   }
